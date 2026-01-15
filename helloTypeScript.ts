961094fe9b46806c35ceb521428a8bf94036c14f
SadikRa <sadikrahman494@gmail.com>
2026-01-15 19:05:55 +0600

    type added done

--- helloTypeScript.ts
+++ helloTypeScript.ts
@@ -1,1 +1,9 @@
 /// well hello welcome to 2026
+
+type person = {
+    name: "sadik";
+    age: 24;
+    language: 'arabic , bangla, urdu, hindi, english';
+    job: 'non';
+    skill: 'jack of all trader'
+}
