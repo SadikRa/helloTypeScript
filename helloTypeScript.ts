4c3f7053b7be1a46ed55a38474f0d1ce472ab087
SadikRa <sadikrahman494@gmail.com>
2026-02-28 11:39:02 +0600

    hello 28 february

--- helloTypeScript.ts
+++ helloTypeScript.ts
@@ -1,28 +1,29 @@
 /// well hello welcome to 2026
 
 type person = {
-    name: "sadik";
-    age: 24;
-    language: 'arabic , bangla, urdu, hindi, english';
-    job: 'non';
-    skill: 'jack of all trader'
-}
+  name: "sadik";
+  age: 24;
+  language: "arabic , bangla, urdu, hindi, english";
+  job: "non";
+  skill: "jack of all trader";
+};
 
 interface target {
-    course: 'dars a nazami';
-    subject: 'quran , hadith, fiqh, arabic grammar, arabic language, usool a quran, usool a hadith, aqeedah, seerah, history, locgic, ect';
-    duration: 'life time';
-    
+  course: "dars a nazami";
+  subject: "quran , hadith, fiqh, arabic grammar, arabic language, usool a quran, usool a hadith, aqeedah, seerah, history, locgic, ect";
+  duration: "life time";
 }
 
-
-
-function greet (name: string): string{
-    return`hello, ${name}`
+function greet(name: string): string {
+  return `hello, ${name}`;
 }
 
 console.log(greet("sadik"));
 
-
 // let message: string = 'Hello World from TypeScript!';
 // console.log(message);
+
+
+/// well hello 28 february
+
+
